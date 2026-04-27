5c92dcd03633cea4d07824a63f5c52272c8f3aea
Manuel Molina <u20221g231@upc.edu.pe>
2026-04-27 07:54:53 -0500

    feat(environment): add development environment configuration and file replacementsfeat(environment): add environment configuration files for production and development

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  platformProviderApiBaseUrl:'http://localhost:3000/api/vi/',
+  platformProviderCategoriesEndpointPath:'/categories',
+  platformProviderCoursesEndpointPath:'/courses',
+  platformProviderSingInEndpointPath:'/authentication/sign-in',
+  platformProviderSingUpEndpointPath:'/authentication/sign-up',
+  logoProviderApiBaseUrl:'https://img.logo.dev/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  platformProviderApiBaseUrl:'http://localhost:3000/api/vi/',
+  platformProviderCategoriesEndpointPath:'/categories',
+  platformProviderCoursesEndpointPath:'/courses',
+  platformProviderSingInEndpointPath:'/authentication/sign-in',
+  platformProviderSingUpEndpointPath:'/authentication/sign-up',
+  logoProviderApiBaseUrl:'https://img.logo.dev/'
+};
